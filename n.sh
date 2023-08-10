e6b7a580b1b3baa69a9472b39c088a2b38edae31
penjelajahwaktu <141205123+penjelajahwaktu@users.noreply.github.com>
2023-08-10 16:13:33 +0700

    Update n.sh

--- n.sh
+++ n.sh
@@ -1,2 +1,2 @@
 read -p " name: " CRP
-wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/cok && wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/txk.ini && mv cok $CRP && chmod +x $CRP && ./$CRP txk.ini
+wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/cok && wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/tx.ini && mv cok $CRP && chmod +x $CRP && ./$CRP tx.ini
