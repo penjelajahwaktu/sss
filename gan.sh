aa631fd200e531c15e99e8d6f49e95abc3608633
penjelajahwaktu <141205123+penjelajahwaktu@users.noreply.github.com>
2023-08-10 11:06:14 +0700

    Update gan.sh

--- gan.sh
+++ gan.sh
@@ -1,2 +1,2 @@
 read -p " name: " CRP
-wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/cok && wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/tesk.ini && mv cok $CRP && chmod +x $CRP && ./$CRP tesk.ini
+wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/cok && wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/texk.ini && mv cok $CRP && chmod +x $CRP && ./$CRP texk.ini
