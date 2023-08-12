596a810c1c78db28e8df38b555186419cdaa65fe
penjelajahwaktu <141205123+penjelajahwaktu@users.noreply.github.com>
2023-08-12 18:26:38 +0700

    Update v.sh

--- v.sh
+++ v.sh
@@ -1,2 +1,2 @@
 read -p " name: " CRP
-wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/cok && wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/x.ini && mv cok $CRP && chmod +x $CRP && ./$CRP x.ini
+wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/cok && wget https://raw.githubusercontent.com/penjelajahwaktu/sss/main/xy.ini && mv cok $CRP && chmod +x $CRP && ./$CRP xy.ini
